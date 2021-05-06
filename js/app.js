b167c8d42924dfc8f1a6d256bdcb69948442ea1b
Jean <jrc.lemaire@gmail.com>
2021-05-05 21:43:04 -0700

    requete url

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 const $recipesName = document.querySelector('.recipes-name')
 
 
-
-fetch("/assets/recipes.json")
+fetch("assets/recipes.json")
     .then((res) => res.json())
     .then((data) => {
         const recipes = data.recipes
 
+        let recipe = ""
         // Méthode à la papa
         for (let i = 0; i < recipes.length; i++) {
-            console.log(recipes[i])
+            recipe += recipes[i].name
             // Prochaine étape faire 
         }
+        $recipesName.innerHTML = `<li>${recipe}</li>`
     })
+    
     .catch(() => console.log("==="))
