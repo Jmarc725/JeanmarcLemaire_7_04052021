593bc14ce82b9c6d8a1bcc07f0d3f53a45fbf836
Jean <jrc.lemaire@gmail.com>
2021-05-11 13:48:58 -0700

    2e solution extraction recipes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-const $recipesName = document.querySelector('.recipes-name')
-const $recipesTime = document.querySelector('.recipes-time')
-const $recipesIngredients = document.querySelector('.recipes-ingredients')
-const $recipesDescription = document.querySelector('.recipes-description')
+// const $recipesName = document.querySelector('.recipes-name')
+// const $recipesTime = document.querySelector('.recipes-time')
+// const $recipesIngredients = document.querySelector('.recipes-ingredients')
+// const $recipesDescription = document.querySelector('.recipes-description')
 
 // let $eachRecipe = document.createElement('div')
 // let $allRecipes = document.querySelector('.cards-grid')
 // $allRecipes.appendChild.$eachRecipe
 
-let recipe = document.querySelector('.wrapper-recipe')
+// let recipe = document.querySelector('.wrapper-recipe')
+
+let contentHtml = ''
 
 fetch("assets/recipes.json")
     .then((res) => res.json())
@@ -21,9 +23,17 @@ fetch("assets/recipes.json")
         //     $recipesDescription.innerHTML = `<li>${recipes[i].description}</li>`
         // }
 
+        // for(let i = 0; i < recipes.length; i++){
+        //     recipe.innerHTML += `<ul><li>${recipes[i].name}</li><li>${recipes[i].time}</li><li>${recipes[i].description}</li></ul>`
+        // }
+
         for(let i = 0; i < recipes.length; i++){
-            recipe.innerHTML += `<ul><li>${recipes[i].name}</li><li>${recipes[i].time}</li><li>${recipes[i].description}</li></ul>`
+            let recipe = recipes[i]
+            contentHtml += `<h2 class="recipe-name">${recipe.name}</h2>
+                            <p class="recipe-time">${recipe.time}</p>
+                            <p class="recipe-description">${recipe.description}</p>`
         }
+        document.querySelector('main').innerHTML = contentHtml
 
        
 
