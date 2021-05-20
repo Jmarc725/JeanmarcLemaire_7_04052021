c7c0c67b3ae9e28bc8801e360f76d50936187816
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-05-20 18:37:03 +0200

    Session du 20 mai 2021

--- js/app.js
+++ js/app.js
@@ -5,22 +5,44 @@ function createRecipeCards(recipes) {
     // Parcourt ton tableau de recettes (50)
     for (let i = 0; i < recipes.length; i++) {
         // ingredientsContentHtml réinitialisé à chaque tour de boucle et prend pour valeur <ul>
-        let ingredientsContentHtml = '<ul>'
+        let ingredientsList = '<ul>'
 
         // console.log(recipes[i].time)
 
         // à chaque tour de boucle, je rajoute le li avec le nom de mon ingrédient
         // Parcourt ton tableau d'ingrédients pour chaque recette
         for (let y = 0; y < recipes[i].ingredients.length; y++) {
-            ingredientsContentHtml += `<li>
-            ${recipes[i].ingredients[y].ingredient} 
-            ${recipes[i].ingredients[y].quantity} 
-            ${recipes[i].ingredients[y].unit}
-            </li>`
+            let ingredient = '<li>'
+
+            if (recipes[i].ingredients[y].quantity !== undefined) {
+                ingredient += recipes[i].ingredients[y].quantity
+            }
+
+            ingredient += ' '
+
+            if (recipes[i].ingredients[y].unit !== undefined) {
+                ingredient += recipes[i].ingredients[y].unit
+            }
+
+            ingredient += ' '
+            
+
+            ingredient += recipes[i].ingredients[y].ingredient // tomate
+            
+            
+
+            // ingredientsContentHtml += `<li>
+            //     ${recipes[i].ingredients[y].ingredient} 
+            //     ${recipes[i].ingredients[y].quantity} 
+            //     ${recipes[i].ingredients[y].unit}
+            // </li>`
+            ingredient += '</li>' // <ul><li>tomate</li>
+
+            ingredientsList += ingredient
         }
 
         // là, je ferme la balise ul
-        ingredientsContentHtml += '</ul>'
+        ingredientsList += '</ul>' // <ul><li>fsf</li><li>sfsd</li>
 
         // là, t'injectes tes éléments de recette (nom, temps, description, ingrédients) dedans
         contentHtml += `
@@ -31,7 +53,7 @@ function createRecipeCards(recipes) {
                 <i class="far fa-clock"></i>
                 ${recipes[i].time} min</span>
                 </h2>
-                ${ingredientsContentHtml}
+                ${ingredientsList}
                 <p class="recipe-description">${recipes[i].description}</p>
             </div>
         `
@@ -62,8 +84,9 @@ fetch("assets/recipes.json")
     // }
 // }
     
+        console.log(recipes[0].ingredients)
         console.log(recipes[0].ingredients[4].hasOwnProperty('unit'))
-        console.log(recipes[0].ingredients[4].unit= "40")
+        // console.log(recipes[0].ingredients[4].unit= "40")
         
         // let addMissingProperty = Object.assign(recipes[0].ingredients[4], {unit : ""})
 
